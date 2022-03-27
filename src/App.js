247df4997bb801721a8f97667e0741a5149c1ee0
Daniel Przybylski <danp43@outlook.com>
2022-03-26 20:51:35 -0700

    Complete example

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ function App() {
     type,
   } = useProcessAlert();
 
+  function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  }
+
   // Here is a function that might make a state change to this component,
   // and we want to show the result in our ProcessAlert.
   function testFunction(value) {
-    switch (value) {
-      case 0: setAlert(false); break;
-      case 1: setAlert(true, 'info', 'Application is ready.'); break;
-      case 2: setAlert(true, 'success', 'Damage repaired, Captain.'); break;
-    }
+  if(value!==0){
+      setAlert(true, "working", "We're doing something which requires you to wait until complete...");
+  }
+    sleep(2000).then(() => {
+      switch (value) {
+        case 0: setAlert(false); break;
+        case 1: setAlert(true, 'info', 'Application is ready.'); break;
+        case 200: setAlert(true, 'success', 'Damage repaired, Captain.','Warp power has been restored.'); break;
+        case 404: setAlert(true, 'warning', 'No user was found.'); break;
+        case 500: setAlert(true, 'error', 'This application is not working.','CRM System is down.'); break;
+        case 400: setAlert(true, 'hint', 'Try something different:','1: Either a different way.','2: Or press the button harder.'); break;
+        case 403: setAlert(true, 'notavailable', "You're not authorized to use this application.",'Please contact the help desk.'); break;
+      }
+    })
   }
 
   // Here we return the HTML setting the one type property of our ProcessAlert to the type object 
@@ -32,11 +45,15 @@ function App() {
         <Col>
           <ProcessAlert type={type} />
         </Col>
-        </Row>
+      </Row>
       <Row>
         <Col><Button color='secondary' onClick={() => testFunction(0)}>Hide</Button></Col>
         <Col><Button color='info' onClick={() => testFunction(1)}>Info</Button></Col>
-        <Col><Button color='success' onClick={() => testFunction(2)}>Success</Button></Col>
+        <Col><Button color='success' onClick={() => testFunction(200)}>Success</Button></Col>
+        <Col><Button color='warning' onClick={() => testFunction(404)}>Warning</Button></Col>
+        <Col><Button color='danger' onClick={() => testFunction(500)}>Error</Button></Col>
+        <Col><Button color='light' onClick={() => testFunction(400)}>Hint</Button></Col>
+        <Col><Button color='dark' onClick={() => testFunction(403)}>N/A</Button></Col>
       </Row>
     </Container >
   );
--- src/ProcessAlert.js
+++ src/ProcessAlert.js
@@ -1,7 +1,16 @@
-import { faCircleCheck, faCircleDot, faCircleExclamation, faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleCheck,
+  faCircleDot,
+  faCircleExclamation,
+  faCircleQuestion,
+  faSun,
+  faMoon,
+  faTriangleCircleSquare,
+  faTriangleExclamation
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
-import { Alert } from 'reactstrap';
+import { Fragment, useState } from 'react';
+import { Alert, Spinner } from 'reactstrap';
 
 // Here is our component which agregates the icons, the Bootstrap Alert and keeps track
 // of what color, icon and text to display.
@@ -9,34 +18,52 @@ export function ProcessAlert(props) {
   const {
     isOpen,
     condition,
-    displayValue
+    displayValue,
+    displayValue1,
+    displayValue2,
+    displayValue3
   } = props.type;
 
   // Our condition is a very limited set. We are intentionally limiting what the consumer
   // can show to keep this simple and reusable for the majority of cases.
-  function getColor(condition) {
+  function getColor() {
     switch (condition) {
-      case "hide": return "info";
-      case "info": return "info";
+      case "hide":
+      case "info":
+      case "working":
+        return "info";
       case "success": return "success";
-      default: return "dark";
+      case "warning": return "warning";
+      case "error": return "danger";
+      case "hint": return "light";
+      case "notavailable":return "dark";
+      default: return "secondary";
     }
   }
 
   // Likewise with our corresponding icons...
-  function getIcon(condition) {
+  function getIcon() {
     switch (condition) {
-      case "hide": return <FontAwesomeIcon icon={faCircleDot} />;
-      case "info": return <FontAwesomeIcon icon={faCircleExclamation} />;
-      case "success": return <FontAwesomeIcon icon={faCircleCheck} />;
+      case "hide": return <FontAwesomeIcon color={getColor()} icon={faCircleDot} />;
+      case "info": return <FontAwesomeIcon color={getColor()} icon={faCircleExclamation} />;
+      case "success": return <FontAwesomeIcon color={getColor()} icon={faCircleCheck} />
+      case "warning": return <FontAwesomeIcon color={getColor()} icon={faTriangleCircleSquare} />
+      case "error": return <FontAwesomeIcon color={getColor()} icon={faTriangleExclamation} />
+      case "hint": return <FontAwesomeIcon color={getColor()} icon={faSun} />
+      case "working": return <Spinner color='info' size='sm' />
+      case "notavailable":return <FontAwesomeIcon color={getColor()} icon={faMoon} />
       default: return <FontAwesomeIcon icon={faCircleQuestion} />;
     }
   }
 
   // Here we return the Alert with our limited number of configuration properties.
   return (
-    <Alert isOpen={isOpen} color={getColor(condition)}>{getIcon(condition)} {displayValue}</Alert>
-  );
+    <Alert color={getColor()} isOpen={isOpen}>
+      {getIcon()} {displayValue}
+      {displayValue1 ? <Fragment> <br />{displayValue1}</Fragment> : null}
+      {displayValue2 ? <Fragment> <br />{displayValue2}</Fragment> : null}
+      {displayValue3 ? <Fragment> <br />{displayValue3}</Fragment> : null}
+    </Alert>);
 }
 
 // And here is our custom hook. 
@@ -44,14 +71,20 @@ export function useProcessAlert() {
   // Notice the states are here and not in the ProcessAlert component?
   const [isOpen, setIsOpen] = useState(false);
   const [displayValue, setDisplayValue] = useState('');
+  const [displayValue1, setDisplayValue1] = useState(undefined);
+  const [displayValue2, setDisplayValue2] = useState(undefined);
+  const [displayValue3, setDisplayValue3] = useState(undefined);
   const [type, setType] = useState('hide');
 
   // And we furnish a convient function to set the ProcessAlert to 
   // one of the few states that we have created for it.
-  function setAlert(isOpen, condition, displayValue) {
+  function setAlert(isOpen, condition, displayValue, displayValue1, displayValue2, displayValue3) {
     setIsOpen(isOpen);
     setType(condition);
     setDisplayValue(displayValue);
+    setDisplayValue1(displayValue1);
+    setDisplayValue2(displayValue2);
+    setDisplayValue3(displayValue3);
   }
 
   // Likewise, we furnish a function to create the various states as one object
@@ -61,7 +94,10 @@ export function useProcessAlert() {
     return {
       isOpen: isOpen,
       condition: type,
-      displayValue: displayValue
+      displayValue: displayValue,
+      displayValue1: displayValue1,
+      displayValue2: displayValue2,
+      displayValue3: displayValue3
     };
   }
 
